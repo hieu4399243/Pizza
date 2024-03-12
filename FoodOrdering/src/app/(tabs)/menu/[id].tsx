b7ae9023e3edb47927851ee56cc83c2271cba4d7
HiYi <hieu243268@gmail.com>
2024-03-11 23:56:00 -0700

    Detail Screen

--- FoodOrdering/src/app/(tabs)/menu/[id].tsx
+++ FoodOrdering/src/app/(tabs)/menu/[id].tsx
@@ -1,18 +1,107 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { PizzaSize } from "@/types";
+import Button from "@/components/Button";
+
+const sizes: PizzaSize[] = ["L", "M", "S", "XL"];
+
+const product = products[0];
 
 const PrductDetails = () => {
-    const {id} = useLocalSearchParams();
+  const [selectedSize, setSelectedSize] = useState("M");
+  const { id } = useLocalSearchParams();
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    console.warn("Add to cart");
+  };
   return (
-    <View>
-      <Text>Product : {id}</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.subtitle}>Select size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            key={size}
+            style={[
+              styles.size,
+              {
+                backgroundColor: size === selectedSize ? "gainsboro" : "white",
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: size === selectedSize ? "black" : "gray" },
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+
+      <Text style={styles.price}> Price: ${product.price.toFixed(2)}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
     </View>
-  )
-}
+  );
+};
 
 export default PrductDetails;
 
 const styles = StyleSheet.create({
-  
-})
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  subtitle: {
+    marginVertical: 10,
+    fontWeight: "600",
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  size: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "500",
+    color: "black",
+  },
+});
---
+++ FoodOrdering/src/app/cart.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text>c</Text>
+    </View>
+  )
+}
+
+export default c
+
+const styles = StyleSheet.create({})
