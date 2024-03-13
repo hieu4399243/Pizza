bc3aed13571114c5de7ad9749ad3824c0d656ab2
HiYi <hieu243268@gmail.com>
2024-03-12 17:29:03 -0700

    Sum product

--- FoodOrdering/src/app/cart.tsx
+++ FoodOrdering/src/app/cart.tsx
@@ -3,9 +3,10 @@ import React, { useContext } from "react";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "./providers/CartProvider";
 import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
   return (
     <View>
       <FlatList
@@ -13,6 +14,8 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       />
+      <Text>Total: ${total}</Text>
+      <Button text="Check out"/>
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
--- FoodOrdering/src/app/providers/CartProvider.tsx
+++ FoodOrdering/src/app/providers/CartProvider.tsx
@@ -7,12 +7,14 @@ type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
   updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
   updateQuantity: () => {},
+  total: 0
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -24,7 +26,7 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         updateQuantity(existItems, 1);
         return;
     }
-    
+
     const newItem = {
       id: randomUUID(),
       product_id: product.id,
@@ -44,9 +46,11 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     const filter = updateItems.filter((item) => item.quantity > 0);
     setItems(filter);
   };
+
+  const total = items.reduce((sum, item) => sum+=item.product.price * item.quantity, 0);
   console.log(items);
   return (
-    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );
